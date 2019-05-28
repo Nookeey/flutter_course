da323b58a360e8d1cd380f90e1947f1a51a79352
nookeey <eernestt@o2.pl>
2019-05-28 16:17:01 +0200

    b03_c01

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-import 'pages/home.dart';
+import './pages/auth.dart';
+import './pages/products_admin.dart';
+import './pages/products.dart';
+import './pages/product.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+  List<Map<String, String>> _products = [];
+
+  void _addProduct(Map<String, String> product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
+  void _deleteProduct(int index) {
+    setState(() {
+     _products.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +37,32 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.deepPurple,
       ),
       title: 'Flutter Course',
-      home: HomePage(),
+      // home: AuthPage(),
+      routes: {
+        '/' : (BuildContext contenx) => ProductsPage(_products, _addProduct, _deleteProduct),
+        '/admin' : (BuildContext contenx) => ProductsAdminPage(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        final List<String> pathElements = settings.name.split('/');
+        if (pathElements[0] != '') {
+          return null;
+        }
+        if (pathElements[1] == 'product') {
+          final int index = int.parse(pathElements[2]);
+          return MaterialPageRoute<bool>(
+            builder: (BuildContext context) => ProductPage(
+              _products[index]['title'], _products[index]['image']
+            ),
+          );
+        }
+        return null;
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (BuildContext contenx) =>
+            ProductsPage(_products, _addProduct, _deleteProduct),
+        );
+      },
     );
   }
 }
